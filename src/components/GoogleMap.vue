271ee9f580bba52082534cafbf003e7d67467619
hair-cut <kikl003273@naver.com>
2025-04-30 22:03:15 +0900

    必要ない内容削除と簡略化

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,25 +1,27 @@
-<template>
-  <div class="map-container">
-    <!-- gestureHandling : マウスで地図の拡大と縮小可能 -->
-    <GMapMap
-      :center="{ lat: 37.5665, lng: 126.9780 }"
-      :zoom="12"
-      style="width: 100%; height: 100%;"
-      :options="{ gestureHandling: 'greedy' }"
-    />
-  </div>
-</template>
+<script setup>
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 
-<script>
-import { GMapMap } from '@fawmi/vue-google-maps'
-export default {
-  components: { GMapMap }
-}
+// 環境変数(VUE APP方式)
+const googleMapApiKey = process.env.VUE_APP_GOOGLE_MAP_API_KEY
+
+// 起動時に表紙される位置
+const center = { lat: 37.5665, lng: 126.9780 }
+const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
+const pinOptions = { background: '#FBBC04' }
 </script>
 
-<style scoped>
-.map-container {
-  width: 100%;
-  height: 100%;
-}
-</style>
+<template>
+  <GoogleMap
+    :api-key="googleMapApiKey"
+    style="width: 100%; height: 100vh"
+    :center="center"
+    :zoom="15"
+  >
+    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions" />
+    <AdvancedMarker :options="markerOptions">
+      <div style="background: white; color: black; padding: 5px; border-radius: 5px">
+        Custom Content
+      </div>
+    </AdvancedMarker>
+  </GoogleMap>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueGoogleMaps from '@fawmi/vue-google-maps'
 
-const app = createApp(App)
-const GMapAPI = process.env.VUE_APP_GOOGLE_MAP_API_KEY
-
-app.use(VueGoogleMaps, {
-  load: {
-    key: GMapAPI,
-  },
-})
-
-app.mount('#app')
+createApp(App).mount('#app')

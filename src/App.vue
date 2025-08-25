da21e73a66aa712db0663f18323dad658439ea05
hair-cut <kikl003273@naver.com>
2025-08-25 18:43:20 +0900

    login修正-5

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,27 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view></router-view> 
   </div>
 </template>
 
 <script>
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
-import { useRouter } from "vue-router";
 
 const DEMO_EMAIL = process.env.VUE_APP_FIREBASE_DEMO_ACCOUNT;
 const DEMO_PASSWORD = process.env.VUE_APP_FIREBASE_DEMO_PASSWORD;
+
 const DEMO_TOKEN = "2f8a7c1e9b4d";
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('autologin') === DEMO_TOKEN) {
+  signInWithEmailAndPassword(auth, DEMO_EMAIL, DEMO_PASSWORD)
+    .catch(() => {/*  */});
+}
 
 export default {
   name: 'App',
-  setup() {
-    const router = useRouter();
-
-    router.afterEach((to) => {
-      if (to.path === '/autologin') {
-        const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.get('autologin') === DEMO_TOKEN) {
-          signInWithEmailAndPassword(auth, DEMO_EMAIL, DEMO_PASSWORD)
-            .catch(() => {
-
-            });
-        }
-      }
-    });
-
-    return {};
-  },
-};
+}
 </script>
 
 <style>

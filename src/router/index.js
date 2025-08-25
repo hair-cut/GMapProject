8cbb04ca851643108789bf43f0d320ccfeb81316
hair-cut <kikl003273@naver.com>
2025-08-25 18:52:45 +0900

    login修正-6

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,49 @@
+// router/index.js
 import { createRouter, createWebHashHistory } from 'vue-router';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+const DEMO_EMAIL = process.env.VUE_APP_FIREBASE_DEMO_ACCOUNT;
+const DEMO_PASSWORD = process.env.VUE_APP_FIREBASE_DEMO_PASSWORD;
+const DEMO_TOKEN = "2f8a7c1e9b4d";
+
 import Login from '../views/Login.vue';
 import Signup from '../views/Signup.vue'; 
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
 
 const routes = [
   { path: '/login', name: 'Login', component: Login },
-  { path: '/signup', name: 'Signup', component: Signup},
-  { path: '/', name: 'HomePage', component: HomePage }
+  { path: '/signup', name: 'Signup', component: Signup },
+  { path: '/', name: 'HomePage', component: HomePage },
+  { path: '/autologin', name: 'AutoLogin', component: HomePage } 
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   base: '/GMapProject/',
   routes
+});
+
+// autologin 処理
+router.beforeEach((to, from, next) => {
+  if (to.path === '/autologin') {
+
+    const token = to.query.token;
+
+    if (token === DEMO_TOKEN) {
+      signInWithEmailAndPassword(auth, DEMO_EMAIL, DEMO_PASSWORD)
+        .then(() => {
+          next('/'); 
+        })
+        .catch(() => {
+          next('/login');
+        });
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
